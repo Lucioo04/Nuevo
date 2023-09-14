81c93c8f79514a87d6963763d7c9926b92615f65
Lucioo04 <144745560+Lucioo04@users.noreply.github.com>
2023-09-14 10:34:09 -0300

    cree el archivo

---
+++ probando.js
@@ -0,0 +1,6 @@
+let texto = "Tengo, que ver el que playground es lo mas";
+
+let busqueda = texto.indexOf('playground');
+for (i=busqueda; i<=busqueda +10  ; busqueda++) {
+    console.log (texto[busqueda])
+}
